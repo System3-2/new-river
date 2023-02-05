a107ec14350b69073e8e2d1bda75465e6d3d7dac
OlojaMuyiwa <olojam4969@gmail.com>
2023-02-05 09:39:46 +0100

    finalised

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,13 @@ const About = () => {
       <div className="about-item">
 
         <div className="about-img">
-          <img src="https://cdn.artcld.com/img/w_640,h_640,c_fit/wv0zfx1yolvkgrgdre9s.jpg" alt="about" />
+          <img src="https://pbs.twimg.com/media/FoMJJ22XgAARDVZ?format=jpg&name=small" alt="about" />
         </div>
 
         <div className="about-text">
-          <h3>New River Fine Art is a world class gallery filled with museum quality art.</h3>
+          <h3>Fair Muraic Fine Art is a world class gallery filled with museum quality art.</h3>
           <p>
-            As one of South Florida’s oldest and most prestigious fine art galleries, New River Fine Art presents original
+            As one of South Florida’s oldest and most prestigious fine art galleries, Fair MuraicFine Art presents original
             works and fine prints from Impressionist, 20th Century, Post War and Contemporary Masters,
             Mid-Career and Emerging Artists, making it the premier visual arts gallery in South Florida.
           </p>
@@ -24,7 +24,7 @@ const About = () => {
           </p>
 
           <p>
-            New River Fine Art offers an outstanding schedule of special exhibitions and lectures for patrons.
+            Fair MuraicFine Art offers an outstanding schedule of special exhibitions and lectures for patrons.
             The gallery infuses its impressive selection
             of Masterworks with fresh offerings from contemporary painters,
             sculptors and glass artists to create a collection that is both historically important and current with today’s art market.
@@ -33,7 +33,7 @@ const About = () => {
           <p>
             Our curators
             have years of experience and draw on collections worldwide to acquire the finest
-            works of art for our collectors. New River Fine Art’s clients are assisted by an expert staff dedicated to providing the highest level of service.
+            works of art for our collectors. Fair MuraicFine Art’s clients are assisted by an expert staff dedicated to providing the highest level of service.
           </p>
 
           <p>Whether you are interested in purchasing a special painting or building a private or
